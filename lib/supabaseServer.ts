53c93e94a932d51f8e0ca525cdedf97305346123
Josiah Manzano <jsmav1611travel@gmail.com>
2025-06-11 18:09:43 +0800

    Delete lib directory

--- lib/supabaseServer.ts
+++
@@ -1,7 +0,0 @@
-// lib/supabaseServer.ts (if needed later)
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-
-export const createSupabaseServerClient = () => {
-  return createServerComponentClient({ cookies });
-};
