90f31dde3792f66096d70013ed9cc9cad65c920b
TechDevJosh <jsmav1611travel@gmail.com>
2025-06-18 13:28:10 +0800

    Fix: corrected 'rreact' typo to 'react' in DetailedContactForm

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import FlowerTrail from '../components/FlowerTrail';
 import WindEffect from '../components/WindEffect';
 
 /*
